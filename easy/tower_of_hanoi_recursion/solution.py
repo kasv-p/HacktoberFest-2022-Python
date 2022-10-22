f3cc45f569319a0bf26cc2bdcc004dadf1b8a312
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-10-22 19:46:49 +0530

    updated solution.py all tests passed
    Handled exceptions successfully

--- easy/tower_of_hanoi_recursion/solution.py
+++ easy/tower_of_hanoi_recursion/solution.py
@@ -1,2 +1,14 @@
-def solution():
-    pass
+def solution(n=float('-inf')):
+    if n == float('-inf'):
+        return 0
+    if type(n) == str:
+        try:
+            n = int(n)
+        except:
+            return 0
+
+    if n < 0:
+        return 0
+    if n-int(n) == 0:
+        return 2**n-1
+    return 0
