49bb1f132feb769da5afec2b1b89918a355b46ff
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-10-22 20:14:28 +0530

    Updated half_diamond_pattern.py
    All 8/8 test cases passed

--- easy/half_diamond_pattern/solution.py
+++ easy/half_diamond_pattern/solution.py
@@ -1,2 +1,22 @@
-def solution():
-    pass
+def solution(n=float('-inf')):
+    if n == 0 or n == 1 or n < 0:
+        return -1
+    if n == float('-inf'):
+        return -1
+    N = n//2+1
+    res = ''
+    for i in range(N):
+
+        for j in range(0, i + 1):
+            res += '*'
+        res += '\n'
+
+    # Loop to print the lower half
+    # diamond pattern
+    for i in range(1, N):
+
+        for j in range(i, N):
+            # print("*", end="")
+            res += '*'
+        res += '\n'
+    return res[:-1]
