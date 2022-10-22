9a562cddc1b537b675b69e82e28477e6ee2b8481
kasv-p <84275697+kasv-p@users.noreply.github.com>
2022-10-22 19:08:24 +0530

    Update solution.py

--- easy/binary_search/solution.py
+++ easy/binary_search/solution.py
@@ -1,14 +1,14 @@
 def solution(nums):
     left = 0
-        right = len(nums)-1
-        
-        while left<=right:
-            mid = (left+right)//2
-            if nums[mid]==target:
-                return mid
-            elif nums[mid]>target:
-                right = mid-1
-            else:
-                left = mid+1
-        
-        return -1
+    right = len(nums)-1
+
+    while left<=right:
+        mid = (left+right)//2
+        if nums[mid]==target:
+            return mid
+        elif nums[mid]>target:
+            right = mid-1
+        else:
+            left = mid+1
+
+    return -1
